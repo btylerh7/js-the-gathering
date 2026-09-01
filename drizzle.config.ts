71b1fa64c0fc122e569812e1e382a7b11fd08ae9
Tyler <91493312+btylerh7@users.noreply.github.com>
2026-09-01 13:39:39 -0400

    Add db (#3)
    * move env, add basic db
    
    * start working on schema
    
    * db config
    
    * set up database tables
    
    * fix nuxt config
    
    ---------

---
+++ drizzle.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'drizzle-kit';
+import env from './server/utils/env';
+
+export default defineConfig({
+    dialect: 'turso',
+    schema: './server/db/schema/index.ts',
+    out: './server/db/migrations',
+    dbCredentials: {
+        url: env.TURSO_DATABASE_URL,
+        authToken:
+            env.NODE_ENV == 'development' ? undefined : env.TURSO_AUTH_TOKEN,
+    },
+});
