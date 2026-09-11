71056cd0994cb9238b4793a8f0f505608ff23ead
Tyler <91493312+btylerh7@users.noreply.github.com>
2026-09-11 17:37:07 -0400

    Add Logged in and Logged out pages and Navbar (#6)
    * move index to add-card
    
    * add navbar
    
    * add homepage and login flow

---
+++ app/components/Navbar.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { NuxtLink } from '#components';
+import { authClient } from '#shared/auth-client';
+const session = authClient.useSession();
+
+</script>
+<template>
+	<nav>
+		<NuxtLink to="/">Home</NuxtLink>
+		<NuxtLink v-if="!session.data" to="/sign-in">Sign In</NuxtLink>
+		<p v-if="session.data">Hi, {{ session.data.user.name }}</p>
+		<button v-if="session.data" @click="authClient.signOut()">Sign Out</button>
+	</nav>
+</template>
+
+<style scoped>
+nav {
+	display: flex;
+	align-items: center;
+	justify-content: flex-end;
+	gap: 1em;
+	padding-block: 1em;
+	max-width: 90vw;
+	font-size: 1.25rem;
+
+}
+
+a {
+	text-decoration: none;
+}
+</style>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,5 +1,18 @@
+<script setup lang="ts">
+import { Navbar } from '#components';
+
+</script>
+
 <template>
+	<Navbar />
 	<main>
 		<slot />
 	</main>
 </template>
+<style>
+main {
+	margin: auto;
+	display: grid;
+	place-content: center;
+}
+</style>
