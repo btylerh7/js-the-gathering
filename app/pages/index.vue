618da010c50173917bfc5dd9fc88c4e3bc4a2254
Tyler <91493312+btylerh7@users.noreply.github.com>
2026-09-11 19:23:09 -0400

    add decks page, redirect if not logged in (#8)

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,17 +2,30 @@
 import { useAuthStore } from '~/stores/authStore';
 
 const authStore = useAuthStore();
-
 </script>
 <template>
-	<div v-if="authStore.user">
-		<h1>{{ authStore.user.name }}'s Decks</h1>
-	</div>
-	<div v-else=>
+	<header>
 		<h1>Deck Builder</h1>
-		<p>You must be logged in to view this application. Please
+		<p>
+			A Magic: The Gathering deck builder that can be
+			self-hosted for you and your friends to hate on
+			each other's decks!
+		</p>
+		<button @click="navigateTo('/decks')" style="max-width: fit-content;" v-if="authStore.user">
+			Go to my decks!
+		</button>
+		<p v-else>You must be logged in to view this application. Please
 			<NuxtLink to="/sign-in">sign in</NuxtLink>
 		</p>
-	</div>
+	</header>
 </template>
-<style scoped></style>
+<style scoped>
+header {
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	margin: auto;
+	text-align: center;
+}
+</style>
