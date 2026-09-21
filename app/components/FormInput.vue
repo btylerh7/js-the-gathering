3c28a4e61475921c50e2a8687370680966a168d0
Tyler Baker <internetexplorerbad@robustjumprope.lol>
2026-09-20 21:53:44 -0400

    create custom FormInput

---
+++ app/components/FormInput.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const { label, name } = defineProps<{
+	label: string;
+	name: string;
+}>()
+
+</script>
+<template>
+	<div>
+		<label :for="name">{{ label }}</label>
+		<slot />
+	</div>
+</template>
+
+<style scoped>
+div {
+	display: flex;
+	gap: 1rem;
+}
+</style>
